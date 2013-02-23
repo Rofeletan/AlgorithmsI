721271c628966e67b34c68d5fe3b713f3fa0e0ac
Daniel McKinnon <mckinnon.dm@gmail.com>
2013-02-23 08:56:55 -0500

    Changing tabs to spaces

--- Program1/merge_sort_count_split.py
+++ Program1/merge_sort_count_split.py
@@ -2,51 +2,51 @@
 import argparse
 
 def merge(array1, array2):	
-	i = j = 0
-	output = []
-	
-	for k in range (0, len(array1) + len(array2)):
-		if i != len(array1) and (j == len(array2) or array1[i] < array2[j]):
-			output.append(array1[i])
-			i+=1
-		else:
-			output.append(array2[j])
-			j+=1
-	
-	return output
-	
+  i = j = 0
+  output = []
+  
+  for k in range (0, len(array1) + len(array2)):
+    if i != len(array1) and (j == len(array2) or array1[i] < array2[j]):
+      output.append(array1[i])
+      i+=1
+    else:
+      output.append(array2[j])
+      j+=1
+  
+  return output
+  
 def sort(array):
-	if len(array) == 1:
-		return array
-	
-	x = sort(array[:len(array) / 2])
-	y = sort(array[len(array) / 2:])	
-	z = merge(x,y)
-	return z
-	
+  if len(array) == 1:
+    return array
+  
+  x = sort(array[:len(array) / 2])
+  y = sort(array[len(array) / 2:])	
+  z = merge(x,y)
+  return z
+  
 def merge_and_count_split(array1, array2):	
-	i = j = 0
-	output = []
-	count = 0
-	for k in range (0, len(array1) + len(array2)):
-		if i != len(array1) and (j == len(array2) or array1[i] < array2[j]):
-			output.append(array1[i])
-			i+=1
-		else:
-			output.append(array2[j])
-			j+=1
-			count += len(array1) - i
-	
-	return (output,count)
-	
+  i = j = 0
+  output = []
+  count = 0
+  for k in range (0, len(array1) + len(array2)):
+    if i != len(array1) and (j == len(array2) or array1[i] < array2[j]):
+      output.append(array1[i])
+      i+=1
+    else:
+      output.append(array2[j])
+      j+=1
+      count += len(array1) - i
+  
+  return (output,count)
+  
 def sort_and_count(array):
-	if len(array) == 1:
-		return (array,0)
-	
-	x = sort_and_count(array[:len(array) / 2])
-	y = sort_and_count(array[len(array) / 2:])	
-	z = merge_and_count_split(x[0], y[0])
-	return (z[0], x[1] + y[1] + z[1])
+  if len(array) == 1:
+    return (array,0)
+  
+  x = sort_and_count(array[:len(array) / 2])
+  y = sort_and_count(array[len(array) / 2:])	
+  z = merge_and_count_split(x[0], y[0])
+  return (z[0], x[1] + y[1] + z[1])
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file")
